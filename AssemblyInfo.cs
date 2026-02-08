7960dae04b835e14fd82b9df1ba10241bac5e3e8
Marco Colonna <46967291+70V07@users.noreply.github.com>
2026-02-08 02:18:29 +0100

    Update assembly title and version for SquareCorners

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,14 +1,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SquaredCorner")]
+[assembly: AssemblyTitle("SquareCorners")]
 [assembly: AssemblyDescription("active process to disable Windows 11 rounded corners")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TOVOT")]
-[assembly: AssemblyProduct("SquaredCorner")]
+[assembly: AssemblyProduct("SquareCorners")]
 [assembly: AssemblyCopyright("Copyright © TOVOT 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
